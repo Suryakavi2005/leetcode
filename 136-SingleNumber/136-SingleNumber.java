ff2030b504440395c25efb660568553a006543ad
SURYA KAVI R <suryakavisuryakavi93@gmail.com>
2026-08-11 11:28:17 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,15 @@
+// Last updated: 8/11/2026, 11:28:15 AM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            
+                c=c^nums[i];
+              
+            
+            
+        }
+        return c;
+        
+    }
+}
