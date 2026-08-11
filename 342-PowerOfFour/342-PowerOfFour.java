8127b12dd466d8d251a44c39d1bb12fbc4cec9ce
SURYA KAVI R <suryakavisuryakavi93@gmail.com>
2026-08-11 11:27:11 +0530

    Create 342-PowerOfFour

---
+++ 342-PowerOfFour/342-PowerOfFour.java
@@ -0,0 +1,12 @@
+// Last updated: 8/11/2026, 11:27:10 AM
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n<=0)
+        return false;
+        if(n==1)
+        return true;
+        if(n%4!=0)
+        return false;
+        return isPowerOfFour(n/4);
+    }
+}
